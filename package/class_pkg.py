2b33fcfc4ce091405f28d0c58958e4a474b13f95
raven724 <kangwogus3@gmail.com>
2019-07-31 13:40:04 +0900

    add codes for package class

--- package/class_pkg.py
+++ package/class_pkg.py
@@ -9,15 +9,16 @@ class package:
         self.syspkg = []
         self.pippkg = []
         self.web = []
+        self.info = []
         return None
 
     def check_syspkg(self):
         if self.os_type == 1:
-            self.sys_pkg = debian.pkglist()
+            self.sys_pkg = self.debian.pkglist(self)
         elif self.os_type == 2:
-            self.sys_pkg = fedora.pkglist()
+            self.sys_pkg = self.fedora.pkglist(self)
         elif self.os_type == 3:
-            self.sys_pkg = arch.pkglist()
+            self.sys_pkg = self.arch.pkglist(self)
         else:
             print("Package info can only run at supported Linux.")
             print("Please check your Linux OS type.")
@@ -26,9 +27,9 @@ class package:
 
     def check_pippkg(self):
         if self.os_type == 1:
-            checkpip = debian.check_pkg('python3-pip')
+            checkpip = self.check_existpkg('python3-pip')
         if self.os_type == 2:
-            checkpip = fedora.check_pkg('python36u-pip')
+            checkpip = self.check_existpkg('python36u-pip')
         if checkpip is False:
             self.pip_pkg.append('not installed')
             return 'not installed'
@@ -41,24 +42,38 @@ class package:
     def check_existpkg(self, str_pkg):
         res_check = False
         if self.os_type == 1:
-            list_package = debian.pkglist()
+            list_package = self.debian.pkglist(self)
         elif self.os_type == 2:
-            list_package = fedora.pkglist()
+            list_package = self.fedora.pkglist(self)
         elif self.os_type == 3:
-            list_package = arch.pkglist()
+            list_package = self.arch.pkglist(self)
         else:
-            print("Package info can only run at supported Linux.")
-            print("Please check your Linux OS type.")
-            return None
-        if str_pkg in list_package:
-            res_check = True
+            return err_pkg_os()
+        for sublist in list_package:
+            if str_pkg in sublist:
+                res_check = True
         return res_check
+
+    def check_pkginfo(self, str_pkg):
+        if self.os_type == 1:
+            res_info = self.debian.pkginfo(self, str_pkg)
+        elif self.os_type == 2:
+            res_info = self.fedora.pkginfo(self, str_pkg)
+        elif self.os_type == 3:
+            res_info = self.arch.pkginfo(self, str_pkg)
+        else:
+            return err_pkg_os()
+
+    def err_pkg_os():
+        print("package info is only available at supported Linux type.\
+            --help for more information")
+        return None
 
     def check_firefox(self):
         pass
 
     class debian:
-        def pkglist(self):
+        def pkglist(package):
             pre_package = []
             res_package = []
             info_pkg = os.popen("dpkg -l").read().split('\n')
@@ -68,11 +83,11 @@ class package:
                     for j in list_package:
                         if j is not '':
                             pre_package.append(j)
-            res_package = self.pkgparse(pre_package)
+            res_package = package.debian.pkgparse(pre_package)
             return res_package
 
-        def pkginfo(self, name_pkg):
-            list_package = self.pkglist()
+        def pkginfo(package, name_pkg):
+            list_package = package.debian.pkglist(package)
             res_pkginfo = []
             if name_pkg == 'all':
                 print("warning! dkpg do not support showing specific info for\
@@ -86,14 +101,15 @@ class package:
                         res_pkginfo.append(info_subpkg)
                 return res_pkginfo
             else:
-                if name_pkg in list_package:
+                if package.check_existpkg(name_pkg):
                     info_subpkg = os.popen("dpkg -s " + name_pkg).read()
                     res_pkginfo.append(info_subpkg)
+                    package.info = res_pkginfo
                     return res_pkginfo
                 else:
                     print("No package found. Check input.")
 
-        def pkgparse(self, list_input):
+        def pkgparse(list_input):
             description = 3
             str_description = ''
             res_list = []
@@ -151,10 +167,19 @@ class package:
 
     class arch:
         def pkglist(self):
-            pass
+            res_package = []
+            pre_package = os.popen("pacman -Q").read().split('\n')
+            for i in pre_package:
+                sublist = i.split(' ')
+                res_package.append(sublist)
+            return res_package
 
-        def pkginfo(self):
-            pass
-
-        def pkgparse(self):
-            pass
+        def pkginfo(self, str_pkg):
+            list_package = self.pkglist()
+            res_package = []
+            if str_pkg == 'all':
+                pre_package = os.popen("pacman -Qi").read()
+            else:
+                if str_pkg in list_package:
+                    pre_package = os.popen("pacman -Qi " + str_pkg).read()
+            return pre_package
